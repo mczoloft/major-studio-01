a2a42f76171c96984aaa4c63eafbe95462eef153
Flavio Pessoa <santf655@newschool.edu>
2017-12-05 03:36:03 +0000

    new readme

--- assignments/04UserCases/README.md
+++ assignments/04UserCases/README.md
@@ -1,6 +1,9 @@
 # GDP per capita vs. ECI vs GINI in SubSaharan Africa 
 
-This is the methodology and sources behind this visualisation project.
+## Intro
+
+This is a project where we pick up Chapter 5 from the *Income Inequality Trends in sub-Saharan Africa* and its focus on how to develop African economies (with all its weakness and strenghs). The author chooses the Economic Complexity Index as a tool to understand sub-Saharan countries. The ECI, though, is a black box; even with a whole section trying to unpack its logic, it's a pretty difficult concept. The authors try to use it with other indexes such as the log of GDP as a way to see correlations and understand its behavior, but maybe there's a chance to translate its effects in real world with variables that are closer to real-life experiences. Hence, we intersect the ECI with not only its log of GDP per capita, but also the GINI. Here we have a 3-pointed index, pointing out different traits for each Country: their techincal abilities, their abundance and its effect on society.
+
 
 #### Sketch for the project
 ![alt text](sketch.jpg 'Sketch 2')
--- assignments/04UserCases/script.js
+++ assignments/04UserCases/script.js
@@ -25,127 +25,70 @@ d3.json('CountryData.json', function(error, data) {
     .range([window.innerHeight-30, 30]);
     
     var g =  svg.selectAll('g')
-    .data(data)
-        .enter()
-        .append('g')
-        .style('opacity', 0.2)
-        .on('mouseover', function (){ 
-            d3.select(this)
-            .style('opacity', 1);
-        })
-        .on('mouseout', function (){ 
-            d3.select(this)
-            .style('opacity', 0.2) });
-    
+            .data(data)
+                .enter()
+                .append('g')
+                .on('mouseover', function (){ 
+                    d3.select(this)
+                    .classed('hover', true);
+                })
+                .on('mouseout', function (){ 
+                    d3.select(this)
+                    .classed('hover', false);
+                })
+                .on('click', function (d, i){ 
+                    g.each(function(datum, j) {
+                        d3.select(this)
+                        .classed('active', i === j ? true : false);
+                        // .select('circle')
+                        // .style('visibility', 'visible');
+                    })
+                    
+                });
+
         g.append('line')
                 .attr("class", 'first')
                 .attr("x1", 30)
                 .attr("y1", function (d) {return yScale2(d.GDP_change_2016)})
-                .attr("x2", window.innerWidth*0.31)
-                .attr("y2", function (d) {return yScale1(d.eci_value)})
-                .attr("stroke-width", 1);
+                .attr("x2", window.innerWidth*0.25+30)
+                .attr("y2", function (d) {return yScale1(d.eci_value)});
 
         g.append('line')
                 .attr("class", 'second')
                 .attr("x1", window.innerWidth*0.36)
                 .attr("y1", function (d) {return yScale1(d.eci_value)})
                 .attr("x2", window.innerWidth*0.70)
-                .attr("y2", function (d) {return yScale3(d.Gini)})
-                .attr("stroke-width", 1);
+                .attr("y2", function (d) {return yScale3(d.Gini)});
 
-        var middleLabel = g.append('g');
-
-        middleLabel
+            g
             .append('rect')
             .attr('x', window.innerWidth*0.31)
             .attr("y", function (d) {return yScale1(d.eci_value)})
             .attr('height', 12)
             .attr('width', window.innerWidth*0.36-window.innerWidth*0.31)
-            .attr('fill', 'lightgrey')
-            .attr('stroke', 'white');
-        
-        middleLabel
+            .attr('fill', 'white')
+            .attr('stroke', 'white')
+            .style('opacity', 0);
+
+            g
             .append('text')
                 .attr("class", "labels")
                 .text(function (d) {return d.country_eci})
                 .attr("x", window.innerWidth*0.335)
                 .attr("y", function (d) {return yScale1(d.eci_value)+9})
-                .attr('text-anchor', 'middle')
-                .attr("visibility", "visible");
+                .attr('text-anchor', 'middle');
+
+            g.append('circle')
+                        .attr('x', 100)
+                        .attr('y', 100)
+                        .attr('r', function (d) {return d.Gini})
+                        .style('visibility', 'hidden');
+                        
+                      
 
    });
     
-// d3.json('_Assets/CountryData.json', function(error, data) {
-//     if (error) throw error;
-
-//     CountryData.push(data);
-//     console.log(CountryData);
-    
-//     var svg = d3.select('#viz1')
-//     .append('svg')
-//     .style('width', window.innerWidth*0.75)
-//     .style('height', window.innerHeight-10);
-    
-//     var yScale1 = d3.scaleLinear()
-//     .domain([d3.min(data, function (d) { return d.eci_value}), d3.max(data, function(d) { return d.eci_value; })])
-//     .range([window.innerHeight-10, 0]);
-    
-//     var yScale2 = d3.scaleLinear()
-//     .domain([d3.min(data, function (d) { return d.GDP_change_2016}), d3.max(data, function(d) { return d.GDP_change_2016; })])
-//     .range([window.innerHeight-10, 0]);
-    
-//     var yScale3 = d3.scaleLinear()
-//     .domain([d3.min(data, function (d) { return d.Gini}), d3.max(data, function(d) { return d.Gini; })])
-//     .range([window.innerHeight-10, 0]);
-    
-//         svg.selectAll('line#first')
-//         .data(data)
-//             .enter()
-//             .append("line")
-//                 .attr("x1", 0)
-//                 .attr("id", '#first')
-//                 .attr("y1", function (d) {return yScale2(d.GDP_change_2016)})
-//                 .attr("x2", window.innerWidth*0.31)
-//                 .attr("y2", function (d) {return yScale1(d.eci_value)})
-//                 .attr("stroke-width", 1)
-//                 .attr("stroke", "black");
-
-//         svg.selectAll("line#second")
-//         .data(data)
-//             .enter()
-//             .append("line")
-//                 .attr("x1", window.innerWidth*0.36)
-//                 .attr("id", '#second')
-//                 .attr("y1", function (d) {return yScale1(d.eci_value)})
-//                 .attr("x2", window.innerWidth*0.75)
-//                 .attr("y2", function (d) {return yScale3(d.Gini)})
-//                 .attr("stroke-width", 1)
-//                 .attr("stroke", "black");
-
-//         svg.selectAll("text.labels")
-//         .data(data)
-//             .enter()
-//             .append('rect')
-//             .attr('x', window.innerWidth*0.31)
-//             .attr("y", function (d) {return yScale1(d.eci_value)})
-//             .attr('height', 12)
-//             .attr('width', window.innerWidth*0.36-window.innerWidth*0.31)
-//             .attr('fill', 'black')
-//             .attr('stroke', 'white')
-//             .append("text")
-//                 .attr("class", "labels")
-//                 .text(function (d) {return d.country_eci})
-//                 .attr("x", window.innerWidth*0.335)
-//                 .attr("y", function (d) {return yScale1(d.eci_value)+7})
-//                 .attr('text-anchor', 'middle')
-//                 .attr("visibility", "visible");
-
-//   });
-
 var svg2 = d3.select('#viz2');
-    // .append('svg')
-    // .style('width', window.innerWidth*0.25)
-    // .style('height', window.innerHeight-10);
     
 svg2.selectAll('div')
     .data(IndividualDataTest)
