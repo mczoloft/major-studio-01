2d52985a5e36daa8f769584d483208f63ec3483c
Flavio Pessoa <santf655@newschool.edu>
2017-12-12 06:18:20 +0000

    new files

--- assignments/04UserCases/script.js
+++ assignments/04UserCases/script.js
@@ -9,8 +9,8 @@ d3.json('CountryData.json', function(error, data) {
     
     var svg = d3.select('#viz1')
     .append('svg')
-    .style('width', window.innerWidth*0.75)
-    .style('height', window.innerHeight-10);
+    .attr('width', window.innerWidth*0.75)
+    .attr('height', window.innerHeight-10);
     
     var yScale1 = d3.scaleLinear()
     .domain([d3.min(data, function (d) { return d.eci_value}), d3.max(data, function(d) { return d.eci_value; })])
@@ -36,31 +36,46 @@ d3.json('CountryData.json', function(error, data) {
                     d3.select(this)
                     .classed('hover', false);
                 })
+
                 .on('click', function (d, i){ 
-                    g.each(function(datum, j) {
+                    g.each(function(d, j) {
                         d3.select(this)
                         .classed('active', i === j ? true : false);
-                    })
+                        
+                });
+                        console.log(d); 
+                        
+                        d3
+                        .select('#viz2')
+                        .style('background-color', 'red')
+                        .html('<h2>' + d.country_eci + '</h2>')
+                        .append('svg')
+                        .attr('width', 100)
+                        .attr('height', 100)
+                        .append('circle')
+                        .attr('x', 50)
+                        .attr('y', 50)
+                        .attr('r', function (){return d.Gini})
+                        .attr('fill', 'white');
                         
                     
                 });
 
         g.append('line')
-                .attr("class", 'first')
-                .attr("x1", 30)
-                .attr("y1", function (d) {return yScale2(d.GDP_change_2016)})
-                .attr("x2", window.innerWidth*0.25+30)
-                .attr("y2", function (d) {return yScale1(d.eci_value)});
+            .attr('class', 'first')
+            .attr("x1", 30)
+            .attr("y1", function (d) {return yScale2(d.GDP_change_2016)})
+            .attr("x2", window.innerWidth*0.25+30)
+            .attr("y2", function (d) {return yScale1(d.eci_value)});
 
         g.append('line')
-                .attr("class", 'second')
-                .attr("x1", window.innerWidth*0.36)
-                .attr("y1", function (d) {return yScale1(d.eci_value)})
-                .attr("x2", window.innerWidth*0.70)
-                .attr("y2", function (d) {return yScale3(d.Gini)});
+            .attr("class", 'second')
+            .attr("x1", window.innerWidth*0.36)
+            .attr("y1", function (d) {return yScale1(d.eci_value)})
+            .attr("x2", window.innerWidth*0.70)
+            .attr("y2", function (d) {return yScale3(d.Gini)});
 
-            g
-            .append('rect')
+        g.append('rect')
             .attr('x', window.innerWidth*0.31)
             .attr("y", function (d) {return yScale1(d.eci_value)})
             .attr('height', 12)
@@ -69,34 +84,29 @@ d3.json('CountryData.json', function(error, data) {
             .attr('stroke', 'white')
             .style('opacity', 0);
 
-            g
-            .append('text')
-                .attr("class", "labels")
-                .text(function (d) {return d.country_eci})
-                .attr("x", window.innerWidth*0.335)
-                .attr("y", function (d) {return yScale1(d.eci_value)+9})
-                .attr('text-anchor', 'middle');
-
-            // g.append('circle')
-            //             .attr('x', 100)
-            //             .attr('y', 100)
-            //             .attr('r', function (d) {return d.Gini})
-            //             .style('visibility', 'hidden');
-                        
-                      
+        g.append('text')
+            .attr("class", "labels")
+            .text(function (d) {return d.country_eci})
+            .attr("x", window.innerWidth*0.335)
+            .attr("y", function (d) {return yScale1(d.eci_value)+9})
+            .attr('text-anchor', 'middle');
 
    });
     
-var svg2 = d3.select('#viz2');
+// var svg2 = d3.select('#viz2');
     
-svg2.selectAll('div')
-    .data(IndividualDataTest)
-    .enter()
-    .append('div')
-    .style('display', 'inline-block')
-    .style('width', '5px')
-    .style('height', function (d) {return d*3 + 'px'})
-    .style('background-color', 'black')
-    .style('margin', '2px');
+// svg2.selectAll('div')
+//     .data(IndividualDataTest)
+//     .enter()
+//     .append('div')
+//     .style('display', 'inline-block')
+//     .style('width', '5px')
+//     .style('height', function (d) {return d*3 + 'px'})
+//     .style('background-color', 'black')
+//     .style('margin', '2px');
     
-    console.log(IndividualDataTest);
+    console.log(IndividualDataTest)
+    
+var Countries = [];
+
+console.log(Countries);
