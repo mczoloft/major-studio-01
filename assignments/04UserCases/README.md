3c5ce56959df933860936ce5503e9779245930ac
Flavio Pessoa <santf655@newschool.edu>
2017-12-13 22:13:44 +0000

    new readme

--- assignments/04UserCases/README.md
+++ assignments/04UserCases/README.md
@@ -1,10 +1,25 @@
-# GDP per capita vs. ECI vs GINI in SubSaharan Africa 
+# What does the economic complexity index look like near other indexes?
 
 ## Intro
 
-This is a project where we pick up Chapter 5 from the *Income Inequality Trends in sub-Saharan Africa* and its focus on how to develop African economies (with all its weakness and strenghs). The author chooses the Economic Complexity Index as a tool to understand sub-Saharan countries. The ECI, though, is a black box; even with a whole section trying to unpack its logic, it's a pretty difficult concept. The authors try to use it with other indexes such as the log of GDP as a way to see correlations and understand its behavior, but maybe there's a chance to translate its effects in real world with variables that are closer to real-life experiences. Hence, we intersect the ECI with not only its log of GDP per capita, but also the GINI. Here we have a 3-pointed index, pointing out different traits for each Country: their techincal abilities, their abundance and its effect on society.
+This is a project to focus on how to develop African economies (with all its weakness and strenghs). The Economic Complexity Index is chosen as a tool to understand sub-Saharan countries. The ECI, though, is a black box; even with a whole section trying to unpack its logic, it's complex: it tries to assess how the economy of a country is developed and its potential to breach into other areas of economic activity. 
 
+The UNDP report tries to use it with other indexes such as the log of GDP as a way to see correlations and understand its behavior, but maybe there's a chance to translate its effects in real world with variables that are closer to real-life experiences. Hence, we intersect the ECI with not only its log of GDP per capita, but also the GINI. Here we have a 3-pointed index, pointing out different traits for each Country: their techincal abilities, their abundance and its effect on society.
+
+The visualization has two features: it uses a slopegraph to see how the GDP per capita, ECI and GINI are connected in a country. It also shows, as a secondary feature, the time series for each of of those indexes for the Country selected.
 
 #### Sketch for the project
 ![alt text](sketch.jpg 'Sketch 2')
 https://mczoloft.github.io/major-studio-01/assignments/04UserCases/
+
+#### Sources
+
+World Bank national accounts data, and OECD National Accounts data files.
+
+World Bank, Development Research Group
+
+Povcal
+http://iresearch.worldbank.org/PovcalNet/home.aspx
+
+Observatory of Economic Complexity
+https://atlas.media.mit.edu/en/rankings/country/eci/
--- assignments/04UserCases/script.js
+++ assignments/04UserCases/script.js
@@ -1,30 +1,35 @@
 var CountryData = [];
-var IndividualDataTest = [12, 15, 20, 29, 80, 70, 90, 10, 22, 03, 30, 40, 55, 56, 12, 07];
 
-d3.json('CountryData.json', function(error, data) {
+d3.json('finalformatArray.json', function(error, data) {
     if (error) throw error;
 
     CountryData.push(data);
     
     var svg = d3.select('#viz1')
     .append('svg')
-    .attr('width', window.innerWidth*0.75)
-    .attr('height', window.innerHeight-10);
-    
+    .attr('width', '800')
+    .attr('height', '100%');
     
     console.log(CountryData);
     
     var yScale1 = d3.scaleLinear()
-    .domain([d3.min(data, function (d) { return d.eci_value}), d3.max(data, function(d) { return d.eci_value; })])
-    .range([window.innerHeight-30, 30]);
+    // .domain([d3.min(data, function (d) { return d.LastECI}), d3.max(data, function(d) { return d.LastECI; })])
+    .domain([-2.3,0.2])
+    .range([700, 30]);
     
-    var yScale2 = d3.scaleLinear()
-    .domain([d3.min(data, function (d) { return d.GDP_change_2016}), d3.max(data, function(d) { return d.GDP_change_2016; })])
-    .range([window.innerHeight-30, 30]);
+    console.log('maior ECI: ' + d3.max(data, function(d) { return d.LastECI}));
+    console.log('menor ECI: ' + d3.min(data, function(d) { return d.LastECI}));
+    console.log('maior GINI: ' + d3.max(data, function(d) { return d.LastGINI}));
+    console.log('menor GINI: ' + d3.min(data, function(d) { return d.LastGINI}));
+    
+    var yScale2 = d3.scaleLog()
+    // .domain([d3.min(data, function (d) { return d.LastGDP}), d3.max(data, function(d) { return d.LastGDP; })])
+    .domain([100, 10000])
+    .range([700, 30]);
     
     var yScale3 = d3.scaleLinear()
-    .domain([d3.min(data, function (d) { return d.Gini}), d3.max(data, function(d) { return d.Gini; })])
-    .range([window.innerHeight-30, 30]);
+    .domain([65, 30])
+    .range([700, 30]);
     
     var g =  svg.selectAll('g')
             .data(data)
@@ -49,66 +54,114 @@ d3.json('CountryData.json', function(error, data) {
                         
                         d3
                         .select('#viz2')
-                        .style('background-color', 'red')
-                        .html('<h2>' + d.country_eci + '</h2>')
+                        .style('background-color', '#eae8de')
+                        .html('<h2>' + d.Country_Name + '</h2>')
                         .append('svg')
-                        .attr('width', 100)
-                        .attr('height', 100)
-                        .append('circle')
-                        .attr('x', 50)
-                        .attr('y', 50)
-                        .attr('r', function (){return d.Gini})
-                        .attr('fill', 'white');
+                        .attr('width', 200)
+                        .attr('height', 200)
+                        .append('g')
+                        .attr('class', 'barchart')
+                        .each(function (){
+                            console.log(d);
+                            var chartvalue = [];
+                            chartvalue.push(d.GDP);
+                            
+                            
+                                
+                            d3.select(this)
+                            .selectAll('circle')
+                            .data(chartvalue)
+                            .enter()
+                            .append('circle')
+                            .attr('x', function (datum, k){return (k*20)+20})
+                            .attr('y', 100)
+                            .attr('r', function (d){return d[k]})
+                            .attr('fill', 'white');
+                                
+                            
+                            
+                        });
                         
                     
                 });
 
         g.append('line')
             .attr('class', 'first')
-            .attr("x1", 30)
-            .attr("y1", function (d) {return yScale2(d.GDP_change_2016)})
-            .attr("x2", window.innerWidth*0.25+30)
-            .attr("y2", function (d) {return yScale1(d.eci_value)});
+            .attr("x1", 50)
+            .attr("y1", function (d) {return yScale2(d.LastGDP)})
+            .attr("x2", 350)
+            .attr("y2", function (d) {return yScale1(d.LastECI)});
 
         g.append('line')
             .attr("class", 'second')
-            .attr("x1", window.innerWidth*0.36)
-            .attr("y1", function (d) {return yScale1(d.eci_value)})
-            .attr("x2", window.innerWidth*0.70)
-            .attr("y2", function (d) {return yScale3(d.Gini)});
+            .attr("x1", 450)
+            .attr("y1", function (d) {return yScale1(d.LastECI)})
+            .attr("x2", 750)
+            .attr("y2", function (d) {return yScale3(d.LastGINI)});
 
         g.append('rect')
-            .attr('x', window.innerWidth*0.31)
-            .attr("y", function (d) {return yScale1(d.eci_value)})
+            .attr('x', 345)
+            .attr("y", function (d) {return yScale1(d.LastECI)})
             .attr('height', 12)
-            .attr('width', window.innerWidth*0.36-window.innerWidth*0.31)
+            .attr('width', 28)
             .attr('fill', 'white')
             .attr('stroke', 'white')
             .style('opacity', 0);
 
         g.append('text')
             .attr("class", "labels")
-            .text(function (d) {return d.country_eci})
-            .attr("x", window.innerWidth*0.335)
-            .attr("y", function (d) {return yScale1(d.eci_value)+9})
+            .text(function (d) {return d.Country_Name})
+            .attr("x", 400)
+            .attr("y", function (d) {return yScale1(d.LastECI)+9})
             .attr('text-anchor', 'middle');
+            
+        var YAxisGDP = d3.axisLeft()
+            .scale(yScale2)
+            .ticks(5, '$')
+            .tickValues([100, 500, 1000, 5000, 10000]);
+
+        var YAxisGINI = d3.axisRight()
+            .scale(yScale3)
+            .ticks(5);
+            
+        var padding = 50;
+        
+        svg.append('g')
+        	.attr('class', 'axis')
+        	.attr('transform', 'translate(' + padding + ', 0)')
+        	.call(YAxisGDP);
+        	
+        svg.append('g')
+            .attr('class', 'axis')
+        	.attr('transform', 'translate(750, 0)')
+            .call(YAxisGINI);
+            
+            g.append('line')
+            .attr("x1", 20)
+            .attr("y1", function (d) {return yScale2(d.LastGDP)})
+            .attr("x2", 270)
+            .attr("y2", function (d) {return yScale1(d.LastECI)});
 
    });
-    
-// var svg2 = d3.select('#viz2');
-    
-// svg2.selectAll('div')
-//     .data(IndividualDataTest)
-//     .enter()
-//     .append('div')
-//     .style('display', 'inline-block')
-//     .style('width', '5px')
-//     .style('height', function (d) {return d*3 + 'px'})
-//     .style('background-color', 'black')
-//     .style('margin', '2px');
-    
-    console.log(IndividualDataTest)
     
 var Countries = [];
 
 console.log(Countries);
+
+d3.json('finalformatArray.json', function(error, newdata) {
+if (error) throw error;
+console.log(newdata[0].ECI);
+
+})
+
+// function loglast(){
+    
+//     for (var i = newdata.length; i--; ) {
+//         Things[i];
+//     }
+    
+    
+//     if (true) {
+        
+//     }
+// }
